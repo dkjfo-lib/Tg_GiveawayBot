0d2690691c8e26c35e6e937490b123c2f4688543
Mike <mihailfd@gmail.com>
2022-11-03 13:35:41 +0100

    clean up

--- set_local.cmd
+++
@@ -1,2 +0,0 @@
-powershell -Command "(gc config.py) -replace 'BOT_TOKEN', '5726668567:AAEnt_hRMirItfq2Jrn-srH8OGRz_oVjNOI' | Out-File -encoding ASCII config.py"
-powershell -Command "(gc config.py) -replace 'False', 'True'  | Out-File -encoding ASCII config.py"
