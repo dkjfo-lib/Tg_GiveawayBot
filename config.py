78ece6ea99c1d778f297dc858ac9e02980d0bee1
Mike <mihailfd@gmail.com>
2022-11-03 14:24:18 +0100

    mongo db envvar

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ LOCAL = False
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL") % os.getenv("BOT_TOKEN")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGODB_CONNECTION_STRING = os.getenv("MONGODB_CONNECTION_STRING")
